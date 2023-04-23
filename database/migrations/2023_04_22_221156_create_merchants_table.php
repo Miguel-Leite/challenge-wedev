ec4085c14b20c88a40a14ec1be5b9404844c46a2
Miguel Leite <miguelleite200leite@gmail.com>
2023-04-23 02:29:56 +0100

    ✨feature: the first commit

---
+++ database/migrations/2023_04_22_221156_create_merchants_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->id();
+            $table->string('merchant_name');
+            $table->foreignId('admin_id')->constrained('users','id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('merchants');
+    }
+};

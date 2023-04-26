63ab0a8d610f4860e8651138de5dd4382e7434f2
miguelleite200leite@gmail.com <miguelleite200leite@gmail.com>
2023-04-26 18:18:34 +0100

    chore: 🐳refactoring code

--- tests/Feature/Api/UserControllerTest.php
+++ tests/Feature/Api/UserControllerTest.php
@@ -3,37 +3,37 @@
 namespace Tests\Feature\Api;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Testing\Fluent\AssertableJson;
+use Illuminate\Support\Facades\Artisan;
+use Faker\Factory as Faker;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
-  use RefreshDatabase;
+  use WithFaker;
+
+  public function setUp(): void
+  {
+    parent::setUp();
+
+    // Cria a conexão com o banco de dados em memória (SQLite)
+    $this->app['config']->set('database.default', 'sqlite');
+    $this->app['config']->set('database.connections.sqlite.database', ':memory:');
+    $this->artisan('migrate');
+    
+    // Insere alguns usuários na base de dados
+    User::factory(3)->create();
+  }
+
   /**
    * A basic feature test example.
    */
   public function test_get_users_endpoint(): void
   {
-
-    User::factory(3)->create();
     $response = $this->getJson('/api/users');
-    $response->assertJsonCount(3, 'data');
-    $response->assertStatus(200);
-  }
-
-  public function test_create_(): void
-  {
-
-    $data = [
-      'name'=> 'Miguel',
-      'email'=> 'miguel.leite@gmail.com',
-      'is_admin'=> true,
-      'password'=> '1234',
-    ];
-
-    $response = $this->postJson('/api/users',$data);
-    $response->assertStatus(201);
+    $response->assertStatus(200)
+                 ->assertJsonCount(3, 'data');
   }
 }
